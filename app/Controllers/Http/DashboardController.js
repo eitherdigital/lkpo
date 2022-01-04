b7da84a02fb7bf15f2138d73a2ee7d37b2ad538e
kirillsaint <kirillsaint@wtfuck.tk>
2022-01-05 04:17:34 +0600

    говно залупа пенис хер давалка хуй блядина

--- app/Controllers/Http/DashboardController.js
+++ app/Controllers/Http/DashboardController.js
@@ -3,6 +3,7 @@
 const User = use('App/Models/User')
 const Release = use('App/Models/Release')
 const Database = use('Database')
+const Helpers = use('Helpers')
 
 class DashboardController {
   async getReleases({ view, auth }) {
@@ -14,6 +15,32 @@ class DashboardController {
     async newRelease({ auth,request, response, session }) {
       const release = new Release();
 
+      const validationOptions = {
+        types: ['jpeg'],
+        size: '10mb',
+      }
+      const imageFile = request.file('cover', validationOptions)
+      await imageFile.move(Helpers.publicPath('/covers/'+auth.user.id), {
+        name: auth.user.username+'_'+auth.user.id+'_'+request.input('title')+'_'+request.input('date')+'.jpg',
+        overwrite: true,
+      })
+      if (!imageFile.moved()) {
+        return imageFile.error()
+      }
+
+      const validationOptions2 = {
+        types: ['x-zip-compressed'],
+        size: '100mb',
+      }
+      const zipFile = request.file('tracks', validationOptions2)
+      await zipFile.move(Helpers.publicPath('/tracks/'+auth.user.id), {
+        name: auth.user.username+'_'+auth.user.id+'_'+request.input('title')+'_'+request.input('date')+'.zip',
+        overwrite: true,
+      })
+      if (!zipFile.moved()) {
+        return zipFile.error()
+      }
+
       release.name = request.input('title')
       release.main_artist = request.input('main_artist')
       release.another_artists = request.input('another_artists')
@@ -22,8 +49,8 @@ class DashboardController {
       release.explicit = request.input('explicit')
       release.author = request.input('author')
       release.date = request.input('date')
-      release.link = request.input('link')
-      release.label = request.input('label')
+      release.link = '/tracks/'+auth.user.id+'/'+auth.user.username+'_'+auth.user.id+'_'+request.input('title')+'_'+request.input('date')+'.zip'
+      release.cover = '/covers/'+auth.user.id+'/'+auth.user.username+'_'+auth.user.id+'_'+request.input('title')+'_'+request.input('date')+'.jpg'
       release.accepted = 0
       release.user_id = auth.user.id
 
